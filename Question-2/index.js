766cf17cef949fe8212b95372748f71252146bb4
Prudhvikm <145647429+Prudhvikm@users.noreply.github.com>
2023-10-01 10:37:44 +0530

    Add files via upload
    create resume in Json.format

---
+++ Question-2/index.js
@@ -0,0 +1,34 @@
+let resume={
+    "Basics Information":{
+        "Name":"Prudhvi Krishna",
+        "Father name": "uma maheshware rao",
+    "EmailID":"Prudhvi25id@gmail.com",
+    "MobileNo":"9986689990",
+    "Address":"*-*o-22,MG road,Nehru colony,kavali,spsr nellore,Andhra pradesh-524201"},
+    
+    "Educational Details" :{ 
+        "Post Graduation":{
+            "Institute":"Presidency B-school",
+            "course":"MBA",
+            "Department" : "Marketing & Production",
+            "Year of Passing":"2015"},
+           
+
+    "Under Graduation":{
+      "Institute":"PBR VITS Engineering and Technology",
+      "course":"Bacholre of Enginnering",
+      "Department" : "Mechanical",
+      "Year of Passing":"2013"},},
+
+    
+
+    "Skills":{
+            "Languages":"javascript,MY SQL,C-lang",
+            "Software" : "Power BI,SQL,SSIS,UGNX",
+            "Speaking language":"English,Hindhi,Telugu and kannada"}
+             
+    }
+    
+    {
+    console.log(resume);
+    }
